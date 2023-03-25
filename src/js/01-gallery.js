05c8e6cd4d9f9046a89af3e7015340feb36c641c
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-03-25 23:42:41 +0200

    made task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+// import '/src/css/01-gallery.css';
+// import '/src/css/common.css';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import '/src/css/01-gallery.css';
+import '/src/css/common.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galaryList = document.querySelector('ul.gallery');
+
+const galaryListItems = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+   </a>
+    </li>`
+}).join('');
+
+galaryList.innerHTML = galaryListItems;
+
+
+const galary = new SimpleLightbox('ul.gallery a', {captionDelay: 250, captionsData: 'alt'});
 
 console.log(galleryItems);
