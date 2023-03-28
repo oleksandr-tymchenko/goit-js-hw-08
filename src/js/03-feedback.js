21af71839f3df9daad69d621c3818ed881c53fae
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-03-28 16:23:33 +0300

    completed task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,50 +5,50 @@ const STORAGE_KEY = "feedback-form-state";
 const form = document.querySelector('.feedback-form');
 const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input');
-const formData = {};
-let formFieldsValue = {};
+let formData = {
+    email: '',
+    message: '',
+};
 
-populateFormFields()
+
 
 form.addEventListener('input', throttle(onInputSetValues, 500));
 form.addEventListener('submit', onFormSubmit);
 
+let formFieldsValue = localStorage.getItem(STORAGE_KEY);
+
+if (formFieldsValue) {
+        const restoredData = JSON.parse(formFieldsValue);
+        textarea.value = restoredData.message;
+        input.value = restoredData.email;
+        formData = restoredData;
+}
+
+
+
 function onInputSetValues(e) {
     formData[e.target.name] = e.target.value;
-    // console.log(formData);
-    // if (formData.email === '' && formData.message === '') {
-    //     return;  
-    // }
+    
    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 }
 
-
-function populateFormFields() {
-    
-
-
-    formFieldsValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    // let formEmail = formFieldsValue.email;
-    // let formMessage = formFieldsValue.message;
-   
-
-    // if (formFieldsValue) {
-    //     textarea.value = formFieldsValue.message;
-    //     input.value = formFieldsValue.email;
-
-    // } 
-
-}
 
 function onFormSubmit(e) {
 
     e.preventDefault();
 
-    // if (textarea.value === '' || input.value === '') {
-    //     return;
-    // }
-    console.log(formFieldsValue);
-    formFieldsValue = {};
+    if (textarea.value === '' || input.value === '') {
+        return;
+    }
+    console.dir(formData);
     e.target.reset();
-    localStorage.removeItem(STORAGE_KEY);
+    onSubmitClean();
 }
+
+function onSubmitClean() {
+    
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
+}
+
+
