d068a58a48f4f31eb6e7a2df4507d4ce77e02215
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-03-29 19:08:06 +0300

    fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,16 +20,17 @@ if (STORAGE_KEY) {
 }
 
 
-player.setCurrentTime(pauseTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+player.setCurrentTime(pauseTime)
+//     .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
